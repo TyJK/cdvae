7af1ba00b6f48b11938f8eeb8a599843fb8c462b
Jake White <jake@riskfuel.com>
2022-11-18 10:51:48 -0500

    initial commit of jarvis dev data script

---
+++ scripts/writeout_jarvis_dev.py
@@ -0,0 +1,20 @@
+""" Isolates a subset of Jarvis data for development work """
+
+import pandas as pd
+
+
+def main():
+    print("reading csv...")
+    df = pd.read_csv("~/Projects/mila/cdvae/data/jarvis/train.csv", index_col="dataset_id", nrows=1024)
+    df.to_csv("~/Projects/mila/cdvae/data/jarvis_dev/train.csv")
+
+
+    df = pd.read_csv("~/Projects/mila/cdvae/data/jarvis/val.csv", index_col="dataset_id", nrows=512)
+    df.to_csv("~/Projects/mila/cdvae/data/jarvis_dev/val.csv")
+
+    df = pd.read_csv("~/Projects/mila/cdvae/data/jarvis/test.csv", index_col="dataset_id", nrows=256)
+    df.to_csv("~/Projects/mila/cdvae/data/jarvis_dev/test.csv")
+
+
+if __name__ == "__main__":
+    main()
