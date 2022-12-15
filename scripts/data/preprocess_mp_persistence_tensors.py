59a57b45f12f46247caa05dac1c82687d494412f
Jake White <jake@riskfuel.com>
2022-12-15 12:58:06 -0500

    readded full pi write to mp preprocess script

--- scripts/data/preprocess_mp_persistence_tensors.py
+++ scripts/data/preprocess_mp_persistence_tensors.py
@@ -11,6 +11,7 @@ def main():
 
     all_keys = []
     all_pis = []
+    all_pis_full = []
     for pif in tqdm(pi_files):
         pif = open(os.path.join(pi_dir,pif), "rb")
         pi_dict = pickle.load(pif)
@@ -20,23 +21,27 @@ def main():
         keys.sort()
 
         pis = []
+        pis_full = []
         for key in keys:
             key_pis = pi_dict[key]
             key_pis_plain = np.copy(key_pis[-1,:])  # last channel in BCWH tensor is plain PI
             pis.append(key_pis_plain)
-            del key_pis
+            pis_full.append(key_pis)
+            # del key_pis
 
         all_keys.extend(keys)
         all_pis.append(np.array(pis))
+        all_pis_full.append(np.array(pis_full))
 
     all_keys = np.array(all_keys)
     all_pis = np.concatenate(all_pis).astype(float)
+    all_pis_full = np.concatenate(all_pis_full).astype(float)
 
     with open("/home/jake/Projects/mila/cdvae/data/mp/persistence_tensors/all_keys.npy", "wb") as f:
         np.save(f, all_keys)
 
-    # with open(os.path.join(root,"cdvae/data/qm9/persistence_tensors/all_pis.npy"), "wb") as f:
-    #     np.save(f, all_pis)
+    with open(os.path.join(root,"cdvae/data/qm9/persistence_tensors/all_pis.npy"), "wb") as f:
+        np.save(f, all_pis_full)
 
     with open("/home/jake/Projects/mila/cdvae/data/mp/persistence_tensors/all_pis_plain.npy", "wb") as f:
         np.save(f, all_pis)
