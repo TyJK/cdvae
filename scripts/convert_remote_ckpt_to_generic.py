e21992f31d51c612dbd6d0c93f5f7a305842d8f7
Jake White <jake@riskfuel.com>
2022-12-16 10:55:59 -0500

    accelerated inference script with gpu

--- scripts/convert_remote_ckpt_to_generic.py
+++ scripts/convert_remote_ckpt_to_generic.py
@@ -1,6 +1,6 @@
 import torch
 
-model = torch.load("/home/jake/Projects/mila/cdvae/assets/checkpoints/zinc-plain-pi-epoch=39-step=104759.ckpt")
+model = torch.load("/home/jake/Projects/mila/cdvae/assets/checkpoints/mp-plain-specific-epoch=59-step=54659.ckpt")
 
 # a hack to change full colab-specific paths in ckpts to generic repo paths
 model_hparams_copy = model["hyper_parameters"].copy()
@@ -9,4 +9,4 @@ model_hparams_copy = model_hparams_copy.replace("/content/cdvae/", "./")
 model_hparams_copy = eval(model_hparams_copy)
 model["hyper_parameters"] = model_hparams_copy
 
-torch.save(model, "/home/jake/Projects/mila/cdvae/assets/checkpoints/zinc-plain-pi/epoch=39-step=104759-generic.ckpt")
+torch.save(model, "/home/jake/Projects/mila/cdvae/assets/checkpoints/mp-plain-pi/epoch=59-step=54659.ckpt")
